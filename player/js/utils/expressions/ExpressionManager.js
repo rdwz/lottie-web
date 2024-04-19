ac8ce4a77cc682001da63541c3b0cb9ae521c45f
Marc RDWZ <redwerkz@gmail.com>
2024-04-19 21:20:56 +0200

    🚨  Replace eval

--- player/js/utils/expressions/ExpressionManager.js
+++ player/js/utils/expressions/ExpressionManager.js
@@ -437,8 +437,12 @@ const ExpressionManager = (function () {
 
     var scoped_bm_rt;
     // val = val.replace(/(\\?"|')((http)(s)?(:\/))?\/.*?(\\?"|')/g, "\"\""); // deter potential network calls
-    var expression_function = eval('[function _expression_function(){' + val + ';scoped_bm_rt=$bm_rt}]')[0]; // eslint-disable-line no-eval
-    var numKeys = property.kf ? data.k.length : 0;
+		const createExpressionFunction = (val) => {
+			return function _expression_function(){
+				scoped_bm_rt = $bm_rt;
+				return val;
+			};
+		};
 
     var active = !this.data || this.data.hd !== true;
 
